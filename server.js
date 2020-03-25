fe6434969170f27f974907c86f4e23703503f7e2
tonnon <lucastonnon@gmail.com>
2020-03-25 11:40:15 -0300

    Incluindo Procfile

--- server.js
+++
@@ -1,5 +0,0 @@
-const http = require('http');
-const app = require('./src/index');
-const port = process.env.PORT || 3333;
-const server = http.createServer(app);
-server.listen(port);
--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ const routes = require('./routes');
 
 const app = express();
 
-app,use(cors({
-    origin:'https://be-a-hero.herokuapp.com'
-}));
-/* app,use(cors()); */
+app.use(cors());
 
 app.use(express.json());
 app.use(routes);
